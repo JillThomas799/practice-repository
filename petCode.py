aba61f99753843ffc0caf2f1af9ca2a2ce35c7e3
Jill Thomas <jillthomas532016@outlook.com>
2025-06-07 17:12:01 +0100

    Phase 1 of issueTest2 completion complete

---
+++ petCode.py
@@ -0,0 +1,6 @@
+introduction = "This is a (very short) program to find out a little about your cat..."
+print(introduction)
+catsname = input("Enter your cat's name: ")
+catsage = input("Enter your cat's age: ")
+
+print("Your cat is called " + catsname + ". He is " + str(catsage) + " years old.")
